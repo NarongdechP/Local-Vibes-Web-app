89a6ae1bf00b2287bc35832359415256bc4c23b9
Natkrita26 <natkrita.son@dome.tu.ac.th>
2025-02-13 00:17:56 +0700

    setup vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => { 
+    return ( 
+    <div> 
+    <h1>ยินดีต้อนรับสู่ Local Vibes</h1> 
+    <p>Home</p> 
+    </div> 
+    ) 
+    } 
+    export default Home 
